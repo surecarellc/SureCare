276346e17bfc317f1b27b99896ec0aa278b40807
Rohan Doshi <rohan@Rohans-MacBook-Pro-9.local>
2025-03-03 17:54:05 -0600

    created HelpPage and modified other files to navigate to it

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LaunchPage from "./LaunchPage"; // Your existing LaunchPage component
 import AboutPage from "./AboutPage"; // The new AboutPage component
+import HelpPage from "./HelpPage"; 
 import Questionnaire from "./Questionnaire"; // The updated Questionnaire component
 import SignIn from "./SignIn"; // The SignIn component
 import SignUp from "./SignUp"; // The SignUp component
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LaunchPage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/help" element={<HelpPage />} />
         <Route path="/questionnaire" element={<Questionnaire />} /> {/* Updated Route */}
         <Route path="/signin" element={<SignIn />} /> {/* Added SignIn Route */}
         <Route path="/signup" element={<SignUp />} /> {/* Added SignUp Route */}
