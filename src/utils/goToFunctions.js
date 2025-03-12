916e1a83e864e8abe1d6e8979615b74e02b0cf2f
THEPLANTATION\saatv <saatvik.barla@gmail.com>
2025-03-12 13:22:10 -0500

    factored functions out

---
+++ src/utils/goToFunctions.js
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+export const useNavigation = () => {
+    const navigate = useNavigate();
+
+    return {
+        goToAboutPage: () => navigate("/about"),
+        goToQuestionnairePage: () => navigate("/questionnaire"),
+        goToSignInPage: () => navigate("/signin"),
+        goToHelpPage: () => navigate("/help"),
+        goToLaunchPage: () => navigate("/"),
+        goToResultsPage: () => navigate("/results"),
+        goToSignUpPage: () => navigate("/signup")
+    };
+};
